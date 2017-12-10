5c4bcdff80a285304ee4dbd20cfcbe561b1c68b7
Abdullah Alam <abdullahalam598@gmail.com>
2017-12-10 12:52:45 +0530

    Create Adapter

--- app/src/main/java/com/antimony/scriptoo/syntechx/EventsAdapter.java
+++ app/src/main/java/com/antimony/scriptoo/syntechx/EventsAdapter.java
@@ -1,4 +1,42 @@
 package com.antimony.scriptoo.syntechx;
 
-public class EventsAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class EventsAdapter extends ArrayAdapter<EventClass> {
+
+
+    public EventsAdapter(Context context, ArrayList<EventClass> events){
+        super(context, 0, events);
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+        EventClass event = getItem(position);
+        //GET EVENT FROM ARRAYADAPTER
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_card, parent, false);
+        }
+        //INFLATE THE VIEW
+
+        //POPULATE THE STUFF
+        TextView name = (TextView)convertView.findViewById(R.id.event_card_title);
+        name.setText(event.name);
+
+
+        return super.getView(position, convertView, parent);
+    }
 }
